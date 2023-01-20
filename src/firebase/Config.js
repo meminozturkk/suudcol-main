7e6fa80c1009aeb96f8bb1db17dc1c1e58fcf6ae
meminozturkk <180101051m@gmail.com>
2023-01-20 15:06:09 +0300

    connected firebase

---
+++ src/firebase/Config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBkA0Z3NroiQH0HKyOfqKLpc1Qz2VzPpbI",
+    authDomain: "suudcollection-vue.firebaseapp.com",
+    projectId: "suudcollection-vue",
+    storageBucket: "suudcollection-vue.appspot.com",
+    messagingSenderId: "534234875092",
+    appId: "1:534234875092:web:b9399e72738bca93fbe741",
+    measurementId: "G-NRBWJGQ4JS"
+  };
