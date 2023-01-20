818ce1f7aa6be657120dca093ed37efd90347f96
meminozturkk <180101051m@gmail.com>
2023-01-20 17:47:38 +0300

    firebase yeniden eklendi

---
+++ src/firebase/Config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDiSxADS-Klq41aC_O3HpepLH5jM-nE9m4",
+    authDomain: "suudcollection-bff2c.firebaseapp.com",
+    projectId: "suudcollection-bff2c",
+    storageBucket: "suudcollection-bff2c.appspot.com",
+    messagingSenderId: "738049446121",
+    appId: "1:738049446121:web:52d786bd42a25ee5616078"
+  };
